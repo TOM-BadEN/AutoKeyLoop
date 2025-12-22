4a35d5fb15b62a830bd42d9833c0271a7d186b20
TOM (Bad EN) <1808461873@qq.com>
2025-12-22 08:40:47 +0800

    ovl 修改我要投稿的界面

--- ovl-KeyX/source/macro/store/contribute_ui.cpp
+++ ovl-KeyX/source/macro/store/contribute_ui.cpp
@@ -5,7 +5,7 @@
 using qrcodegen::QrCode;
 using qrcodegen::QrSegment;
 
-static const char* GITHUB_URL = "https://github.com/TOM-BadEN/KeyX-Macro-Repo";
+static const char* GITHUB_URL = "https://github.com/do-kiss/KeyX-Macro-Repository";
 static const char* WECHAT_PAY_URL = "wxp://f2f0mMaZS-xnKyAZaTyn813TQRZHTRKPzI6UWxldiWDYemRIq8Stt_LPfd1sLyV4v1jR";
 static const char* PAYPAL_URL = "https://www.paypal.com/qrcodes/p2pqrc/7CQ7FTPN26AJ8";
 
@@ -25,19 +25,19 @@ tsl::elm::Element* ContributeGui::createUI() {
         // 投稿方式
         r->drawString("投稿方式", false, textX, currentY, 24, titleColor);
         currentY += 34;
-        r->drawString(" • 通过社交软件直接向我投稿", false, textX, currentY, 20, textColor);
+        r->drawString(" • 脚本详情界面点击上传按钮", false, textX, currentY, 20, textColor);
         currentY += lineHeight;
-        r->drawString(" • 只需要提供宏文件和说明即可", false, textX, currentY, 20, textColor);
+        r->drawString(" • 扫描二维码填写脚本信息", false, textX, currentY, 20, textColor);
         currentY += lineHeight;
-        r->drawString(" • 向我的 GitHub 仓库提交 PR", false, textX, currentY, 20, textColor);
+        r->drawString(" • 待审核通过自动上架商店", false, textX, currentY, 20, textColor);
         currentY += 65;
         
-        // 联系方式
-        r->drawString("联系方式", false, textX, currentY, 24, titleColor);
+        // 特别感谢
+        r->drawString("特别感谢", false, textX, currentY, 24, titleColor);
         currentY += 34;
-        r->drawString(" • QQ群: 1051287661", false, textX, currentY, 20, textColor);
+        r->drawString(" • 忘忧 (dokiss.cn)", false, textX, currentY, 20, textColor);
         currentY += lineHeight;
-        r->drawString(" • DC: tom_baden", false, textX, currentY, 20, textColor);
+        r->drawString(" • 感谢提供服务器与制作网页", false, textX, currentY, 20, textColor);
         currentY += 65;
         
         // 两个二维码 - 使用固定 Version 5 (37x37) 保证大小一致
@@ -58,8 +58,8 @@ tsl::elm::Element* ContributeGui::createUI() {
         int startXGithub = textX + 15;
         int startXDonate = x + w - 20 - totalSizeDonate - 15;
         
-        // 双标题：GitHub 仓库（左）  捐赠（右，绿色，居中于二维码）
-        r->drawString("GitHub 仓库", false, textX, currentY, 24, titleColor);
+        // 双标题：脚本仓库（左）  捐赠（右，绿色，居中于二维码）
+        r->drawString("脚本仓库", false, textX, currentY, 24, titleColor);
         
         auto [donateW, donateH] = r->getTextDimensions(donateTitle, false, 24);
         s32 donateTitleX = startXDonate + (totalSizeDonate - donateW) / 2;
